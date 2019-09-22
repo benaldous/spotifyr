9e2a5dc5f981d812e6628578fe564bfc5ad2466a
Ben Aldous <benaldous@Erics-MacBook-Pro.local>
2019-09-22 12:49:02 -0600

    Renamed function

--- R/Batch.R
+++ R/Batch.R
@@ -3,13 +3,13 @@
 #' @param x A tbl
 #' @param col Column to batch
 #' @param n Rows in each group
-#' 
+#'
 #' @import dplyr
-#' @importFrom rlang quo_name
-#' 
+#' @importFrom rlang as_name
+#'
 #' @export
 
-group_n = function(x,col,n) {
+batch_col = function(x,col,n) {
   group_by(x,Group = floor((row_number() - 1)/n)) %>%
     summarize_at(vars(as_name(enquo(col))),list) %>%
     pull()
